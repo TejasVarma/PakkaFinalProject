e5b2abee675684cfcc92355722a7b718a20976a1
Karthik Rao <karthiknrao98@gmail.com>
2020-04-08 18:48:38 +0530

    heyy

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  int SPLASH_SCREEN = 2000;
+
+    //Variables
+    Animation animation;
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView(R.layout.activity_main);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+
+        imageView = findViewById(R.id.imageView2);
+        textView = findViewById(R.id.textView);
+
+        imageView.setAnimation(animation);
+        textView.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
